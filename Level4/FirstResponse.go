00a188883510fe388a272200ac5cf3fbe3e1d8d0
Juan Carlos Kuri Pinto <jckuri@gmail.com>
2020-11-06 11:11:30 -0500

    Create FirstResponse.go

---
+++ Level4/FirstResponse.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Conn struct {
+	id int
+	ms time.Duration
+}
+
+func (conn Conn) String() string {
+	return fmt.Sprintf("Conn {id: %v, ms=%v}", conn.id, conn.ms)
+}
+
+func (conn *Conn) DoQuery() string {
+	time.Sleep(conn.ms * time.Millisecond)
+	return conn.String()
+}
+
+func Query(conns []Conn) string {
+    ch := make(chan string)
+    for _, conn := range conns {
+        go func(c Conn) {
+            select {
+            case ch <- c.DoQuery():
+            default:
+            }
+        }(conn)
+    }
+    return <-ch
+}
+
+func main() {
+	conns := []Conn {
+		Conn {1, 100},
+		Conn {2, 150},
+		Conn {3, 200},
+		Conn {4, 55},
+	}
+	for _, conn := range conns {
+		fmt.Println(conn)
+	}
+	fmt.Println("First response:", Query(conns))
+}
+
+/*
+OUTPUT:
+
+Conn {id: 1, ms=100ns}
+Conn {id: 2, ms=150ns}
+Conn {id: 3, ms=200ns}
+Conn {id: 4, ms=55ns}
+First response: Conn {id: 4, ms=55ns}
+*/
